b4d9cab9810c2876f657673ab7564202cb691506
Jordan Hudgens <jordan@devcamp.com>
2017-07-31 17:36:09 +0000

    implemented session tracker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+  
+  before :set_source
+  
+  def set_source
+    session[:source] = params[:q] if params if [:q]
+  end
 end
